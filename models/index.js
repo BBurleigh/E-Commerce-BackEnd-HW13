adb333d4e77aefe4a459b4289e400d64f97ca6f3
Brayden <brayburl224@gmail.com>
2022-11-15 20:01:55 -0500

    finished drafting index.js

--- models/index.js
+++ models/index.js
@@ -6,20 +6,35 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
-Products.belongsTo(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 })
 
 // Categories have many Products
 
-Category.hasMany(Products, {
+Category.hasMany(Product, {
   foreignKey: 'category_id'
 })
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+    //unique: false
+    // through a unique tag - none of the properties match another tag property
+ 
+})
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
+
+// Product and Tag are not parent/child - need a foreign key to connect one to the other (and vice versa)
 
 module.exports = {
   Product,
